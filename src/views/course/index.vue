004d1b5f44886cdbe9fa906988fb381b534a9aa8
guest3 <email>
2020-04-19 19:13:23 +0800

    修改界面

--- src/views/course/index.vue
+++ src/views/course/index.vue
@@ -75,8 +75,8 @@
 					</div>
 
 					<div style="display: flex;">
-						<el-button type="warning" class="search_btn" @click="seek()">立即查询</el-button>
-						<el-button type="warning" class="search_btn" @click="seekPercentage()">百分比查询</el-button>
+						<el-button type="warning" class="search_btn" @click="seek()">查询</el-button>
+						<el-button type="warning" class="search_btn" @click="seekPercentage()">统计</el-button>
 					</div>
 					<p class="tips_test" v-show="radioStatic == 1">*&nbsp;院校或专业&nbsp;（&nbsp;类&nbsp;）任选其一即可直接查询</p>
 					<p class="tips_test">*&nbsp;选课走班指南仅供参考，请根据实际情况进行选课</p>
@@ -98,17 +98,17 @@
 							<span>当前查询条件为&nbsp;&nbsp;&nbsp;</span>
 							<span v-show="!successQequest && radioStatic == 1">未选择院校或专业&nbsp;(&nbsp;类&nbsp;)</span>
 							<span v-show="!successQequest && radioStatic == 2">未选择首选科目、再选科目</span>
-							
-							<span v-show="successQequest && radioStatic == 1">院校：{{school == '' ? "全部" : school}}; 
+
+							<span v-show="successQequest && radioStatic == 1">院校：{{school == '' ? "全部" : school}};
 							专业(类)：{{profession == '' ? "全部" : profession}}</span>
-							
-							<span v-show="successQequest && radioStatic == 2">首选科目：{{firstSubject == '' ? "不提首选科目要求" : firstSubject}}; 
+
+							<span v-show="successQequest && radioStatic == 2">首选科目：{{firstSubject == '' ? "不提首选科目要求" : firstSubject}};
 							再选科目：{{secondSubject == '' ? "不提再选科目要求" : secondSubject}}</span>
 						</p>
 					</div>
 				</div>
 				<p v-show="!successQequest && radioStatic == 1">请在上方输入院校或专业（类）进行查询</p>
-				<p v-show="!successQequest && radioStatic == 2">请在上方输入首选科目、再选科目进行查询</p>				
+				<p v-show="!successQequest && radioStatic == 2">请在上方输入首选科目、再选科目进行查询</p>
 
 				<!-- 查询结果表格 -->
 				<div v-show="successQequest == 1">
@@ -121,29 +121,29 @@
 						  prop="school"
 						  label="院校">
 						</el-table-column>
-					
+
 						<el-table-column
 						  prop="profession"
 						  label="招生专业(类)">
 						</el-table-column>
-					
+
 						<el-table-column
 						  prop="professionSub"
 						  label="包含专业"
 						  width="350">
 						</el-table-column>
-					
+
 						<el-table-column
 						  prop="subject1"
 						  label="首选科目要求">
 						</el-table-column>
-					
+
 						<el-table-column
 						  prop="subject2"
 						  label="再选科目要求">
 						</el-table-column>
 					</el-table>
-					
+
 					<div class="block">
 						<el-pagination
 							@size-change = "countSizeChange"
@@ -158,11 +158,11 @@
 						</el-pagination>
 					</div>
 				</div>
-				
+
 				<!-- 百分比查询结果 -->
 				<div v-show="successQequest == 2">
 					<div id="echartsId" style="width:970px;height:400px;"></div>
-					
+
 					<el-table
 					:data="perData"
 					stripe
@@ -172,23 +172,23 @@
 						  :prop="radioStatic == 1 ? 'subject1' : 'pro'"
 						  :label="radioStatic == 1 ? '首选科目' : '专业'">
 						</el-table-column>
-					
+
 						<el-table-column
 						  :prop="radioStatic == 1 ? 'subject2' : 'sum'"
 						  :label="radioStatic == 1 ? '再选科目' : '专业类条数'">
 						</el-table-column>
-					
+
 						<el-table-column
 						  :prop="radioStatic == 1 ? 'sum' : 'sumAll'"
 						  :label="radioStatic == 1 ? '符合条件专业数' : '该专业类总条数'">
 						</el-table-column>
-					
+
 						<el-table-column
 						  prop="per"
-						  :label="radioStatic == 1 ? '所占总专业百分比(保留4位小数)' : '专业类占总专业类百分比(保留2位小数)'">
+						  :label="radioStatic == 1 ? '所占总专业百分比(%)' : '专业类占总专业类百分比(%)'">
 						</el-table-column>
 					</el-table>
-					
+
 					<div class="block">
 						<el-pagination
 							@size-change = "perCountSizeChange"
@@ -203,7 +203,7 @@
 						</el-pagination>
 					</div>
 				</div>
-				
+
 			</div>
 		</div>
 
@@ -238,12 +238,12 @@ import util from "@/utils/util.js";
 		firstSubject: '',  //首选科目输入框的值
 		secondSubject: '', //再选科目输入框的值
 		successQequest: false, //控制搜索结果显示
-		
+
 		tableData: [],  //普通搜索结果
 		currentPage: 1,  //当前页数
 		dataNum: null, //表格数据总数
 		count: 30,  //每页数据条数
-		
+
 		perData: [],  //百分比搜索结果
 		perCurrentPage: 1,  //当前页数
 		perDataNum: null, //表格数据总数
@@ -337,8 +337,8 @@ import util from "@/utils/util.js";
 							axisTick: {
 								alignWithLabel: true
 							},
-							axisLabel:{  
-								interval:0,//横轴信息全部显示  
+							axisLabel:{
+								interval:0,//横轴信息全部显示
 								                clickable:true,//并给图表添加单击事件  根据返回值判断点击的是哪里
 								                formatter : function(params,index){
 								                    if (index % 2 != 0) {
@@ -401,7 +401,7 @@ import util from "@/utils/util.js";
 			let getPer = [];  //存放专业类占总专业类百分比
 			let newPer = [];  //存放组装数据
 			api.getProfessionPer(data, (res)=>{
-				let data = api.getData(res);  
+				let data = api.getData(res);
 				let dataCopy = data.slice(0, 10) //获取前十条数据
 				//处理前十条数据(柱状图用)
 				dataCopy.forEach((item, index) => {
@@ -411,7 +411,7 @@ import util from "@/utils/util.js";
 					num = num.toFixed(2);  //保留二位小数(数据类型会被改变为string类型)
 					getPer.push(parseFloat(num));  //数据类型转为number类型并添加
 				});
-				
+
 				//处理全部数据(表格用)
 				data.forEach((item, index) =>{
 					let num = (parseInt(item.sum) * 100 ) / parseInt(item.sumAll);  //计算专业类占总专业类百分比
@@ -423,7 +423,7 @@ import util from "@/utils/util.js";
 					newObj.per = parseFloat(num);
 					newPer.push(newObj);
 				});
-				
+
 				this.perData = newPer;
 				this.perDataNum = res.data.total;
 				this.getChat(getSum, getPer, getName);
